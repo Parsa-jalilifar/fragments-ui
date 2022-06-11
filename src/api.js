636bb7f996c793fd526d6df58b7650cd1454143f
Parsa-jalilifar <parsajalilifar@gmail.com>
2022-06-11 16:39:47 -0400

    Add getFragmentData

--- src/api.js
+++ src/api.js
@@ -15,11 +15,14 @@ export async function getUserFragments(user) {
       // Generate headers with the proper Authorization bearer token to pass
       headers: user.authorizationHeaders(),
     });
+
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
+
     const data = await res.json();
     console.log('Got user fragments data', { data });
+    if(data['fragments'].length != 0)  getFragmentData(user, data['fragments'].at(-1));
   } catch (err) {
     console.error('Unable to call GET /v1/fragment', { err });
   }
@@ -37,5 +40,25 @@ export async function postUserFragment(user) {
     });
   } catch (err) {
     console.error("Unable to call POST /v1/fragment");
+  }
+}
+
+export async function getFragmentData(user, id) {
+  console.log(`Requesting fragment data by id: ${id}`);
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.text();
+    console.log(data);
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragments/:id", { err });
   }
 }

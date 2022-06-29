08be177ef7c9b7a3306398d1aa518a24077c62a6
Parsa Jalilifar <parsajalilifar@gmail.com>
2022-06-29 11:31:05 -0400

    Add create feature (A2 support)

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 // src/app.js
 
-import { Auth, getUser } from "./auth";
-import { getUserFragments, postUserFragment } from "./api";
+import { Auth, getUser } from './auth';
+import { getUserFragments, postUserFragment } from './api';
 
 async function init() {
   // Get our UI elements
-  const userSection = document.querySelector("#user");
-  const loginBtn = document.querySelector("#login");
-  const logoutBtn = document.querySelector("#logout");
-  const addFragmentBtn = document.getElementById("add-fragment");
-  const fragmentSection = document.getElementById("fragment-section");
+  const loginBtn = document.querySelector('#login');
+  const logoutBtn = document.querySelector('#logout');
+
+  const userSection = document.querySelector('#user');
+
+  const fragmentSection = document.getElementById('fragment-section');
+  const fragmentsTypes = document.getElementById('fragments-types');
+  const fragmentData = document.getElementById('fragment-data');
+  const addFragmentBtn = document.getElementById('add-fragment');
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
@@ -36,21 +40,21 @@ async function init() {
   console.log({ user });
 
   addFragmentBtn.onclick = () => {
-    postUserFragment(user);
+    postUserFragment(user, fragmentsTypes.value, fragmentData.value);
   };
 
   // Update the UI to welcome the user
   userSection.hidden = false;
 
   // Show the user's username
-  userSection.querySelector(".username").innerText = user.username;
+  userSection.querySelector('.username').innerText = user.username;
 
   // Disable the Login button
   loginBtn.disabled = true;
 
   // Do an authenticated request to the fragments API server and log the result
-  getUserFragments(user);
+  getUserFragments(user, 1);
 }
 
 // Wait for the DOM to be ready, then start the app
-addEventListener("DOMContentLoaded", init);
+addEventListener('DOMContentLoaded', init);
